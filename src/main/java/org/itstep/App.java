904e8dbd585db3510f222a91dcae44e37c7ce814
shabrat2015 <shabrat2015@gmail.com>
2021-01-03 00:55:26 +0200

    ....

--- src/main/java/org/itstep/App.java
+++ src/main/java/org/itstep/App.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("-->");
+        System.out.println(">>>");
         TestComand comand = new TestComand();
-        comand.tok=scanner.nextLine();
+        comand.tok=scanner.next();
         comand.returnComand().exp();
     }
 }
