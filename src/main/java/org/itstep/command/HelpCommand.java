8fec59694fbd814f05828e4fa22728d8e052026d
shabrat2015 <shabrat2015@gmail.com>
2021-01-03 01:06:55 +0200

    sdg

--- src/main/java/org/itstep/command/HelpCommand.java
+++ src/main/java/org/itstep/command/HelpCommand.java
@@ -3,6 +3,6 @@ package org.itstep.command;
 public class HelpCommand implements Command{
     @Override
     public void execute(String... s) {
-        System.out.print("Help executed");
+        System.out.println("Help executed");
     }
 }
